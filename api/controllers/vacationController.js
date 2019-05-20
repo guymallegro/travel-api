aa4a67bc2c4cd685c9c46bad427153e33c68dcac
Guy Marushchenko <guym@allegro.ai>
2019-05-20 14:55:25 +0300

    add the DB

--- api/controllers/vacationController.js
+++ api/controllers/vacationController.js
@@ -1,21 +1,23 @@
 'use strict';
+let DButilsAzure = require('./DButils');
 
-exports.login = function(req, res) {
-  res.send("Given user name is: "+req.body.userName);
+
+exports.login = function (req, res) {
+    res.send("Given user name is: " + req.body.userName);
 };
 
-exports.list_all_tasks = function(req, res) {
-  res.send("TEST");
+exports.list_all_tasks = function (req, res) {
+    res.send("TEST");
 };
 
-exports.list_all_tasks = function(req, res) {
-  res.send("TEST");
+exports.list_all_tasks = function (req, res) {
+    res.send("TEST");
 };
 
-exports.list_all_tasks = function(req, res) {
-  res.send("TEST");
+exports.list_all_tasks = function (req, res) {
+    res.send("TEST");
 };
 
-exports.list_all_tasks = function(req, res) {
-  res.send("TEST");
+exports.list_all_tasks = function (req, res) {
+    res.send("TEST");
 };
