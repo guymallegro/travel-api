04aa79384f66d4d3df261e930918f1ce9e3c5163
Guy Marushchenko <guym@allegro.ai>
2019-05-29 00:26:49 +0300

    General improvements

--- api/controllers/vacationController.js
+++ api/controllers/vacationController.js
@@ -1,6 +1,5 @@
 'use strict';
 let DButilsAzure = require('../../DButils');
-var User = require('../models/user');
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
@@ -10,41 +9,54 @@ let countries = ["Australia", "Bolivia", "China", "Denemark", "Israel", "Latvia"
     "Panama", "Switzerland", "USA"]
 
 exports.register = function (req, res) {
-    if (req.body.userName.length < 3 || req.body.userName.length > 8){
-        res.status(400).send("user name should be between 3 and 8 chars.")
+    if (!req.body.userName || !req.body.password || !req.body.firstName || !req.body.lastName || !req.body.country ||
+        !req.body.city || !req.body.email || !req.body.firstQuestion || !req.body.secondQuestion || !req.body.firstAnswer
+        || !req.body.secondAnswer || !req.body.firstInterest || !req.body.secondInterest) {
+        res.status(400).send("The request is invalid, the required fields are : userName, password, firstName, lastName," +
+            " country, city, email, firstInterest, secondInterest, firstQuestion, secondQuestion, firstAnswer, secondAnswer");
         return;
     }
-    if (!(/^[a-zA-Z]+$/.test(req.body.userName))){
-        res.status(400).send("user name should contains letters only.")
+    if (req.body.userName.length < 3 || req.body.userName.length > 8) {
+        res.status(400).send("user name should be between 3 and 8 chars.");
         return;
     }
-    if (req.body.password.length < 5 || req.body.password.length > 10){
-        res.status(400).send("password should be between 5 and 10 chars.")
+    if (!(/^[a-zA-Z]+$/.test(req.body.userName))) {
+        res.status(400).send("user name should contains letters only.");
         return;
     }
-    if (!req.body.password.match("^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z0-9]+$")){
-        res.status(400).send("password should contains both letters and numbers.")
+    if (req.body.password.length < 5 || req.body.password.length > 10) {
+        res.status(400).send("password should be between 5 and 10 chars.");
         return;
     }
-    if(!countries.includes(req.body.country)){
-        res.status(400).send("The given country is not supported.")
+    if (!req.body.password.match("^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z0-9]+$")) {
+        res.status(400).send("password should contains both letters and numbers.");
+        return;
+    }
+    if (!countries.includes(req.body.country)) {
+        res.status(400).send("The given country is not supported.");
         return;
     }
     DButilsAzure.execQuery("INSERT INTO Users (userName, password, firstName, lastName, country, city, email)" +
         "VALUES ('" + req.body.userName + "','" + req.body.password + "','" + req.body.firstName + "','" + req.body.lastName + "','" +
         req.body.country + "','" + req.body.city + "','" + req.body.email + "') " +
         "INSERT INTO UsersInterests (userName, firstInterest, secondInterest) " +
-        "VALUES ('" + req.body.userName + "','" + req.body.firstInterest + "','" + req.body.secondInterest +"')")
+        "VALUES ('" + req.body.userName + "','" + req.body.firstInterest + "','" + req.body.secondInterest + "') " +
+        "INSERT INTO UsersQuestions (userName, firstQuestion, firstAnswer, secondQuestion, secondAnswer) " +
+        "VALUES ('" + req.body.userName + "','" + req.body.firstQuestion + "','" + req.body.firstAnswer + "','" +
+        req.body.secondQuestion + "','" + req.body.secondAnswer + "')")
         .then(function (result) {
-            res.send(result)
+            res.send(result);
         })
         .catch(function (err) {
-            console.log(err)
-            res.send(err)
-        })
+            res.status(400).send("A user with the same username already exists.");
+        });
 };
 
 exports.login = function (req, res) {
+    if (!req.body.userName || !req.body.password) {
+        res.status(400).send("The request is invalid, the required fields are : userName, password");
+        return;
+    }
     DButilsAzure.execQuery("SELECT * FROM Users \n" +
         "WHERE (userName = '" + req.body.userName + "') AND (password = '" + req.body.password + "')")
         .then(function (result) {
@@ -54,35 +66,37 @@ exports.login = function (req, res) {
                 const token = jwt.sign(payload, secret, options);
                 res.send(token);
             } else {
-                res.status(400).send("Invalid username or password")
+                res.status(400).send("The given password/username is incorrect.")
             }
         })
         .catch(function (err) {
-            console.log(err)
-            res.send(err)
+            console.log(err);
+            res.status(400).send("One of the input values is invalid.")
         })
 };
 
-exports.verifyAnswer = function (req, res) {
+exports.verifyUserAnswer = function (req, res) {
+    if (!req.body.userName || !req.body.firstAnswer || !req.body.secondAnswer) {
+        res.status(400).send("The request is invalid, the required fields are : userName, firstAnswer, secondAnswer");
+        return;
+    }
     DButilsAzure.execQuery("SELECT * FROM UsersQuestions WHERE (userName='" + req.body.userName + "') AND" +
-        "(firstQuestion='" + req.body.firstQuestion + "') AND (firstAnswer='" + req.body.firstAnswer + "')")
+        " (firstAnswer='" + req.body.firstAnswer + "') AND (secondAnswer='" + req.body.secondAnswer + "')")
         .then(function (result) {
             if (result == 0) {
-                console.log(result);
                 res.send("Incorrect answer. Please try again");
             } else {
-                console.log(result);
                 DButilsAzure.execQuery("SELECT password FROM Users WHERE (userName='" + req.body.userName + "')")
                     .then(function (pass) {
                         res.send(pass);
                     })
                     .catch(function (err) {
-                        res.send(err)
+                        res.status(400).send("One of the input values is invalid.")
                     })
             }
         })
         .catch(function (err) {
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 };
 
@@ -98,8 +112,7 @@ exports.getUserRecommendation = function (req, res) {
             res.send(result)
         })
         .catch(function (err) {
-            console.log(err)
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 };
 
@@ -107,33 +120,43 @@ exports.getUserFavorites = function (req, res) {
     let userName = auth(req, res)
     DButilsAzure.execQuery("SELECT * FROM POI " +
         "JOIN UsersFavoritePOI " +
-        "ON poiName = point " +
+        "ON POI.poiName = UsersFavoritePOI.point " +
         "WHERE (userName='" + userName + "')")
         .then(function (result) {
             res.send(result)
         })
         .catch(function (err) {
-            console.log(err)
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 };
 
-exports.updateUser = function (req, res) {
+exports.updateUserInfo = function (req, res) {
+    if (!req.body.password || !req.body.firstName || !req.body.lastName || !req.body.country || !req.body.city ||
+        !req.body.email || !req.body.firstQuestion || !req.body.firstAnswer || !req.body.secondQuestion || !req.body.secondAnswer) {
+        res.status(400).send("The request is invalid, the required fields are : password, firstName, lastName, country," +
+            " city, email, firstInterest, secondInterest, firstQuestion, firstAnswer, secondQuestion, secondAnswer");
+        return;
+    }
     let userName = auth(req, res)
     DButilsAzure.execQuery("UPDATE Users " +
         "SET password ='" + req.body.password + "', firstName = '" + req.body.firstName + "', lastName = '" +
         req.body.lastName + "', city = '" + req.body.city + "', country = '" + req.body.country + "', email = '" + req.body.email + "'" +
+        "WHERE (userName='" + userName + "') " +
+        "UPDATE UsersInterests " +
+        "SET firstInterest = '" + req.body.firstInterest + "', secondInterest = '" + req.body.secondInterest + "' " +
+        "WHERE (userName='" + userName + "') " +
+        "UPDATE UsersQuestions " +
+        "SET firstQuestion = '" + req.body.firstQuestion + "', secondQuestion = '" + req.body.secondQuestion + "', firstAnswer = '" + req.body.firstAnswer + "', secondAnswer = '" + req.body.secondAnswer + "' " +
         "WHERE (userName='" + userName + "')")
         .then(function (result) {
             res.send(result)
         })
         .catch(function (err) {
-            console.log(err)
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 };
 
-exports.getUser = function (req, res) {
+exports.getUserInfo = function (req, res) {
     let userName = auth(req, res)
     DButilsAzure.execQuery("SELECT * FROM Users " +
         "WHERE (userName='" + userName + "')")
@@ -141,32 +164,42 @@ exports.getUser = function (req, res) {
             res.send(result)
         })
         .catch(function (err) {
-            console.log(err)
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 };
 
 exports.getPOIDetails = function (req, res) {
+    if (!req.body.poiName) {
+        res.status(400).send("The request is invalid, the required fields are : poiName");
+        return;
+    }
     DButilsAzure.execQuery("Select distinct POI.category, POI.description, POI.watchedAmount, POI.rank, POI.image,\n" +
         "POIReviews.dateFirstReview, POIReviews.firstReview, POIReviews.dateSecondReview,\n" +
         "POIReviews.secondReview\n" +
         "from POI join POIReviews on POIReviews.poiName=POI.poiName and POI.poiName='" + req.body.poiName + "'")
         .then(function (result) {
-            res.send(result);
+            if (result == 0)
+                res.send("The given POI doesn't exist.")
+            else
+                res.send(result);
         })
         .catch(function (err) {
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 };
 
 exports.getUserQuestions = function (req, res) {
-    DButilsAzure.execQuery("SELECT * FROM UsersQuestions\n" +
+    if (!req.body.userName) {
+        res.status(400).send("The request is invalid, the required fields are : userName");
+        return;
+    }
+    DButilsAzure.execQuery("SELECT firstQuestion, secondQuestion FROM UsersQuestions\n" +
         "WHERE (userName = '" + req.body.userName + "')")
         .then(function (result) {
             res.send(result)
         })
         .catch(function (err) {
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 }
 
@@ -179,114 +212,135 @@ exports.getAllPOI = function (req, res) {
             res.send(result)
         })
         .catch(function (err) {
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 }
 
-exports.addReview = function (req, res) {
+exports.addPOIReview = function (req, res) {
+    if (!req.body.reviewIndex || !req.body.review || !req.body.date || !req.body.poiName) {
+        res.status(400).send("The request is invalid, the required fields are : poiName, review, date, reviewIndex(1 or 2)");
+        return;
+    }
     auth(req, res);
-    if (req.body.firstReview) {
+    if (req.body.reviewIndex === 1) {
         DButilsAzure.execQuery("UPDATE POIReviews " +
             "SET firstReview = '" + req.body.review + "', dateFirstReview = '" + req.body.date + "'" +
-            "WHERE (poiName='" + req.body.poi + "')")
+            "WHERE (poiName='" + req.body.poiName + "')")
             .then(function (result) {
                 res.send(result)
             })
             .catch(function (err) {
-                res.send(err)
+                res.status(400).send("One of the input values is invalid.")
             })
     } else {
         DButilsAzure.execQuery("UPDATE POIReviews " +
             "SET secondReview = '" + req.body.review + "', dateSecondReview = '" + req.body.date + "'" +
-            "WHERE (poiName='" + req.body.poi + "')")
+            "WHERE (poiName='" + req.body.poiName + "')")
             .then(function (result) {
                 res.send(result)
             })
             .catch(function (err) {
-                res.send(err)
+                res.status(400).send("One of the input values is invalid.")
             })
     }
 
 }
 
-exports.addFavoritePOI = function (req,res) {
+exports.addFavoritePOI = function (req, res) {
+    if (!req.body.poiName) {
+        res.status(400).send("The request is invalid, the required fields are : poiName");
+        return;
+    }
     let userName = auth(req, res);
     DButilsAzure.execQuery("INSERT INTO UsersFavoritePOI " +
-        "VALUES ('"+userName+"','" + req.body.poi + "')")
+        "VALUES ('" + userName + "','" + req.body.poiName + "')")
         .then(function (result) {
             res.send(result)
         })
         .catch(function (err) {
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 }
 
 exports.removeFavoritePOI = function (req, res) {
+    if (!req.body.poiName) {
+        res.status(400).send("The request is invalid, the required fields are : poiName");
+        return;
+    }
     let userName = auth(req, res);
     DButilsAzure.execQuery("DELETE FROM UsersFavoritePOI " +
-        "WHERE (userName = '" + userName + "') AND (point = '" + req.body.poi + "')")
+        "WHERE (userName = '" + userName + "') AND (point = '" + req.body.poiName + "')")
         .then(function (result) {
             res.send(result)
         })
         .catch(function (err) {
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 }
 
 exports.setUserRank = function (req, res) {
-    if (req.body.rank < 1 || req.body.rank > 5){
+    let userName = auth(req, res)
+    if (!req.body.poiName || !req.body.rank) {
+        res.status(400).send("The request is invalid, the required fields are : poiName, rank");
+        return;
+    }
+    if (req.body.rank < 1 || req.body.rank > 5) {
         res.status(400).send("rank should be between 1 and 5.")
         return;
     }
-    DButilsAzure.execQuery("SELECT rank FROM UsersRanks WHERE (userName='" + req.body.userName + "') AND" +
+    DButilsAzure.execQuery("SELECT rank FROM UsersRanks WHERE (userName='" + userName + "') AND" +
         " (poiName='" + req.body.poiName + "')")
         .then(function (result) {
             if (result == 0) {
                 DButilsAzure.execQuery("INSERT INTO usersRanks (userName, poiName, rank) VALUES " +
-                    "('"+req.body.userName+ "','"+req.body.poiName+"','"+req.body.rank+"')")
+                    "('" + userName + "','" + req.body.poiName + "','" + req.body.rank + "')")
                     .then(function (pass) {
                         res.send(pass);
                     })
                     .catch(function (err) {
-                        res.send(err)
+                        res.status(400).send("One of the input values is invalid.")
                     })
             } else {
-                DButilsAzure.execQuery("UPDATE usersRanks SET rank='"+req.body.rank+"' WHERE poiName='"+req.body.poiName+
-                    "' AND userName='"+req.body.userName+"'")
+                DButilsAzure.execQuery("UPDATE usersRanks SET rank='" + req.body.rank + "' WHERE poiName='" + req.body.poiName +
+                    "' AND userName='" + userName + "'")
                     .then(function (pass) {
                         res.send(pass);
                     })
                     .catch(function (err) {
-                        res.send(err)
+                        res.status(400).send("One of the input values is invalid.")
                     })
             }
         })
         .catch(function (err) {
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 }
 
-exports.getAllRanks = function (req, res) {
+exports.getAllPOIRanks = function (req, res) {
     DButilsAzure.execQuery("SELECT * FROM usersRanks")
         .then(function (result) {
             res.send(result)
         })
         .catch(function (err) {
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 }
 
 exports.updatePOIRank = function (req, res) {
-    if (req.body.rank < 1 || req.body.rank > 5){
+    if (!req.body.poiName || !req.body.rank) {
+        res.status(400).send("The request is invalid, the required fields are : poiName, rank");
+        return;
+    }
+    if (req.body.rank < 1 || req.body.rank > 5) {
         res.status(400).send("rank should be between 1 and 5.")
         return;
     }
-    DButilsAzure.execQuery("UPDATE POI SET rank='"+req.body.rank+"' WHERE poiName='"+req.body.poiName+"'")
+    DButilsAzure.execQuery("UPDATE POI SET rank='" + req.body.rank + "' WHERE poiName='" + req.body.poiName + "'")
         .then(function (pass) {
             res.send(pass);
         })
         .catch(function (err) {
-            res.send(err)
+            res.status(400).send("One of the input values is invalid.")
         })
 }
 
--- api/routes/vacationRoutes.js
+++ api/routes/vacationRoutes.js
@@ -2,39 +2,52 @@
 module.exports = function (app) {
     var vacation = require('../controllers/vacationController');
 
-    app.route('/login')
+    app.route('/users/login')
         .post(vacation.login);
 
-    app.route('/register')
+    app.route('/users/register')
         .post(vacation.register);
 
-    app.route('/verifyAnswer')
-        .post(vacation.verifyAnswer);
+    app.route('/users/verifyAnswer')
+        .post(vacation.verifyUserAnswer);
 
-    app.route('/getUserRecommendation')
+    app.route('/users/getRestoreQuestions')
+        .post(vacation.getUserQuestions);
+
+    app.route('/users/getRecommendation')
         .post(vacation.getUserRecommendation);
 
-    app.route('/getUserFavorites')
+    app.route('/users/getFavorites')
         .post(vacation.getUserFavorites);
 
-    app.route('/getUser')
-        .post(vacation.getUser);
+    app.route('/users/getInfo')
+        .post(vacation.getUserInfo);
 
-    app.route('/updateUser')
-        .put(vacation.updateUser);
+    app.route('/users/updateInfo')
+        .put(vacation.updateUserInfo);
 
-    app.route('/addReview')
-        .put(vacation.addReview);
-
-    app.route('/updatePOIRank')
-        .put(vacation.updatePOIRank);
-
-    app.route('/setUserRank')
-        .put(vacation.setUserRank);
-
-    app.route('/addFavoritePOI')
+    app.route('/users/addFavoritePOI')
         .put(vacation.addFavoritePOI);
 
-    app.route('/removeFavoritePOI')
+    app.route('/users/removeFavoritePOI')
         .delete(vacation.removeFavoritePOI);
+
+    app.route('/users/rankPOI')
+        .put(vacation.setUserRank);
+
+    app.route('/POI/getDetails')
+        .post(vacation.getPOIDetails);
+
+    app.route('/POI/getAll')
+        .post(vacation.getAllPOI);
+
+    app.route('/POI/addReview')
+        .put(vacation.addPOIReview);
+
+    app.route('/POI/updateRank')
+        .put(vacation.updatePOIRank);
+
+    app.route('/POI/getAllRanks')
+        .put(vacation.getAllPOIRanks);
+
 };
